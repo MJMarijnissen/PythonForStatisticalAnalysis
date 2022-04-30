a92204c2779d6e8561c641871f57cf1f68766b64
MJMarijnissen <kubamarijnissen@gmail.com>
2022-04-30 21:36:48 +0200

    loaded data using np.genfromtxt , pd.read_csv and pickle

--- Exploring Data Analysis/ExDataAnal.py
+++ Exploring Data Analysis/ExDataAnal.py
@@ -23,9 +23,25 @@ with open(filename) as f:
             data.append([float(x)for x in vals])
             
 d0 = pd.DataFrame(data, columns=cols)
-d0.head()
+print(d0.head())
 
 
 #%% 
 d1 = np.loadtxt(filename, skiprows=1, delimiter=",")
 print(d1[:5,:])
+
+#%% 
+d2 = np.genfromtxt(filename, delimiter=',', names=True, dtype=None)
+print(d2['A'][:5])
+#print(d2[:5])
+
+#%%
+d3 = pd.read_csv(filename)
+print(d3.head())
+
+#%%
+with open("load_pickle.pickle","rb") as f:
+    d4 = pickle.load(f)
+    
+print(d4.dtypes)
+print(d4.head())
