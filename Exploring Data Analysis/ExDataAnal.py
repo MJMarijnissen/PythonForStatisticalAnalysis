267970a41968d211ef45b67e9d4762507d2be736
MJMarijnissen <kubamarijnissen@gmail.com>
2022-04-30 21:19:03 +0200

    loaded file with open(filename) as f:

--- Exploring Data Analysis/ExDataAnal.py
+++ Exploring Data Analysis/ExDataAnal.py
@@ -8,3 +8,18 @@ Created on Sat Apr 30 21:02:40 2022
 import numpy as np
 import pickle 
 import pandas as pd
+
+filename = "load.csv"
+
+cols = None
+data = []
+with open(filename) as f:
+    for line in f.readlines():
+        vals = line.replace("\n",'').split(",")
+        if cols is None:
+            cols = vals
+        else:
+            data.append([float(x)for x in vals])
+            
+d0 = pd.DataFrame(data, columns=cols)
+d0.head()
