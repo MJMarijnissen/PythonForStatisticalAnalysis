7a018cca765a1460bc325c5770cebf388612e804
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-02 11:43:51 +0200

    cleaned data

--- Exploring Data Analysis/prepare.py
+++ Exploring Data Analysis/prepare.py
@@ -9,3 +9,15 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv("Diabetes.csv")
+
+df = df.fillna(0)
+#df.dropna()
+
+df2 = df[["Glucose", "BMI", "Age", "Outcome"]]
+
+print(df2.describe())
+
+#deleting all rows that have 0 in them (~ inverses the relation): 
+df3 = df2.loc[~(df2[df2.columns[:-1]] == 0).any(axis=1)]
+
+print(df3.describe())
