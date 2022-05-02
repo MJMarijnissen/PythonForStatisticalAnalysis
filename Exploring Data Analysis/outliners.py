ac5ffc2ae6c0f7c6606fc17aad596466a0f09903
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-02 12:31:08 +0200

    created pltos for imported data

--- Exploring Data Analysis/outliners.py
+++ Exploring Data Analysis/outliners.py
@@ -12,3 +12,9 @@ d1 = np.loadtxt("outlier_1d.txt")
 d2 = np.loadtxt("outlier_2d.txt")
 d3 = np.loadtxt("outlier_curve.txt")
 print(d1.shape,d2.shape)
+
+#created data points with contamination
+plt.scatter(d1, np.random.normal(7, 0.2, size=d1.size), s=1, alpha=0.5)
+plt.scatter(d2[:,0],d2[:,1])
+plt.show()
+plt.plot(d3[:,0],d3[:,1])
