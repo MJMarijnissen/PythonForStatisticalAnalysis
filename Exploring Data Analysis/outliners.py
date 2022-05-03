2db41656cc45485b03c0918e064ae9d0dc897425
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-03 12:52:05 +0200

    Indentified outliners for d1 using z_score

--- Exploring Data Analysis/outliners.py
+++ Exploring Data Analysis/outliners.py
@@ -18,3 +18,20 @@ plt.scatter(d1, np.random.normal(7, 0.2, size=d1.size), s=1, alpha=0.5)
 plt.scatter(d2[:,0],d2[:,1])
 plt.show()
 plt.plot(d3[:,0],d3[:,1])
+
+
+#%%
+#Rejection of outliners based on standard distribution and Z-score
+
+mean, std = np. mean(d1), np.std(d1)
+z_score = np.abs((d1-mean)/std)
+threshold = 3
+good = z_score < threshold
+
+print(f"Rejection {(~good).sum()} points")
+from scipy.stats import norm
+print(f"z_score of 3 corresponds to a prob of {100*2*norm.sf(threshold):0.2f}%")
+visual_scatter = np.random.normal(size=d1.size)
+plt.scatter(d1[good], visual_scatter[good], s=2, label="Good", color="#4CAF50")
+plt.scatter(d1[~good], visual_scatter[~good], s=2, label="Bad", color="#F44336")
+plt.legend()
