60530bbc33faed94d9cf1cf4dccc232ba7227f6c
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-03 14:39:13 +0200

    Indentified outliners for curve using polyfit/polyval

--- Exploring Data Analysis/outliners.py
+++ Exploring Data Analysis/outliners.py
@@ -47,3 +47,32 @@ good = mn(mean, cov).pdf(d2) > 0.01 /100
 plt.scatter(d2[good, 0], d2[good, 1], s=2, label="Good", color="#4CAF50")
 plt.scatter(d2[~good, 0], d2[~good, 1], s=2, label="Bad", color="#F44336")
 plt.legend()
+
+#%%
+#Rejection of curve outliners using polyfit
+
+xs, ys = d3.T
+p = np.polyfit(xs, ys, deg=5)
+ps = np.polyval(p, xs)
+
+plt.plot(xs, ys, ".", label = "Data")
+plt.plot(xs, ps, label = "Bad poly fit")
+plt.legend()
+
+x, y = xs.copy(), ys.copy()
+for i in range(5):
+    p = np.polyfit(x, y, deg=5)
+    ps = np.polyval(p,x)
+    good = y- ps <3 #no negative outliners, no need for abs
+    
+    x_bad, y_bad, = x[~good], y[~good]
+    x, y = x[good], y[good]
+    
+    plt.plot(x, y, ".", label ="Used Data")
+    plt.plot(x, np.polyval(p,x), label=f"Poly fit {i}")
+    plt.plot(x_bad, y_bad, ".", label="Not used Data")
+    plt.legend()
+    plt.show()
+    
+    if (~good).sum() == 0:
+        break
