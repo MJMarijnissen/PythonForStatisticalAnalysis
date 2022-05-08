9bd147f96e6ad6233b08c338ec35b6f835f0e99f
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-08 20:12:35 +0200

    readied imports

--- Exploring Data Analysis/NDDataExploration.py
+++ Exploring Data Analysis/NDDataExploration.py
@@ -5,3 +5,11 @@ Created on Sun May  8 20:09:09 2022
 @author: Kuba
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+import pandas as pd
+
+df_original = pd.read_csv("Diabetes.csv")
+print(df_original.head())
+
