a1d3806d0616ddd220c2f2931a883af0da14dbf2
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-08 20:18:02 +0200

    finished data prep (changing 0 to NaN

--- Exploring Data Analysis/NDDataExploration.py
+++ Exploring Data Analysis/NDDataExploration.py
@@ -13,3 +13,8 @@ import pandas as pd
 df_original = pd.read_csv("Diabetes.csv")
 print(df_original.head())
 
+#Data prep
+cols = [c for c in df_original.columns if c not in ["Pregnancies", "Outcome"]]
+df = df_original.copy()
+df[cols] = df[cols].replace({0: np.NaN})
+#pandas will ignore NaNs in mean ect. calculations
