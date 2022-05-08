ca307e878d792a1c22b25ba8b069a3d335962eb1
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-08 18:38:05 +0200

    Created imports for histograms

--- Exploring Data Analysis/1Dhistograms.py
+++ Exploring Data Analysis/1Dhistograms.py
@@ -5,3 +5,11 @@ Created on Sun May  8 18:34:10 2022
 @author: Kuba
 """
 
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn
+import pandas as pd
+
+d1 = np.loadtxt("example_1.txt")
+d2 = np.loadtxt("example_2.txt")
+print(d1.shape, d2.shape)
