b7cc74205b650c1cb03f30aa9874ec42ea5babe1
MJMarijnissen <kubamarijnissen@gmail.com>
2022-05-08 20:35:57 +0200

    finished scatter matrix

--- Exploring Data Analysis/NDDataExploration.py
+++ Exploring Data Analysis/NDDataExploration.py
@@ -18,3 +18,9 @@ cols = [c for c in df_original.columns if c not in ["Pregnancies", "Outcome"]]
 df = df_original.copy()
 df[cols] = df[cols].replace({0: np.NaN})
 #pandas will ignore NaNs in mean ect. calculations
+
+#%% SCATTER MATRIX
+
+df2=df.dropna()
+colors = df2["Outcome"].map(lambda x: "#44d9ff" if x else "#f95b4a")
+pd.plotting.scatter_matrix(df, figsize=(7,7), color=colors)
